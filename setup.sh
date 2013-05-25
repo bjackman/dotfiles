b4ea002dc8042636b3d9174992b4d161d3627472
Brendan <jackmab1@cs.man.ac.uk>
2013-05-25 17:55:57 +0100

    added a message

--- setup.sh
+++ setup.sh
@@ -8,5 +8,6 @@ fi
 if [[ -e ~/.my_bashrc ]]; then
   echo "~/.my_bashrc already exists, could not create link"
 else
+  echo "linking ~/my_bashrc to ~/dotfiles/.my_bashrc"
   ln -s ~/dotfiles/.my_bashrc ~/.my_bashrc
 fi
