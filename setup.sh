8d0c75f5b9d605d1adff9f8fab446a1fa2920caf
Brendan <jackmab1@cs.man.ac.uk>
2013-05-25 16:10:36 +0100

    yerrrrrrrrr

--- setup.sh
+++ setup.sh
@@ -1,1 +1,12 @@
-ln -s dotfiles/.my_bashrc .my_bashrc
+if [[ -n `grep "my_bashrc" ~/.bashrc` ]]; then
+  echo "bashrc already source my_bashrc"
+else
+  echo "adding 'source ~/.my_bashrc' to ~/.bashrc"
+  echo "source ~/.my_bashrc" >> ~/.bashrc
+fi
+
+if [[ -e ~/.my_bashrc ]]; then
+  echo "~/.my_bashrc already exists, could not create link"
+else
+  ln -s ~/dotfiles/.my_bashrc ~/.my_bashrc
+fi
