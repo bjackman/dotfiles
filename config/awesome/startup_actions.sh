afafb4e362d9a2374b5390db06c824a02a2c19a8
Brendan Jackman <bhenryj0117@gmail.com>
2014-10-29 20:11:08 +0000

    startup actions

--- config/awesome/startup_actions.sh
+++ config/awesome/startup_actions.sh
@@ -4,5 +4,7 @@ setxkbmap gb # Set keyboard map
 xset m 4 4	 # Set mouse sensitivity
 awsetbg -c ~/.config/awesome/Earth-terminator.jpg
 
-pgrep conky > /dev/null || conky
-pgrep nm-applet > /dev/null || nm-applet
+pgrep conky > /dev/null || conky&
+pgrep nm-applet > /dev/null || nm-applet&
+
+SpiderOak&
