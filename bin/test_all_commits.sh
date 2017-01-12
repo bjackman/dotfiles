ee38aaa42290c7725bab77be543f938301c0ece9
Brendan Jackman <brendan.jackman@arm.com>
2017-01-12 11:51:41 +0000

    Add help to test_all_commits.sh

--- bin/test_all_commits.sh
+++ bin/test_all_commits.sh
@@ -1,5 +1,12 @@
 set -eu
 
+if [ $1 == '-h' ]; then
+    echo "To run a command at every commit between <base> and HEAD:"
+    echo "- Set TEST_COMMIT_CMD to the command you want to run"
+    echo "- $0 <base>"
+    exit 0
+fi
+
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
 for commit_id in $(git log --format=%H $1..HEAD); do
