bc89edb595dce13fb082868dfadb42794092601b
Brendan Jackman <jackmanb@chromium.org>
2023-12-17 18:38:31 +0100

    Add basic git info to fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,3 +1,10 @@
+set __fish_git_prompt_show_informative_status 1
+set __fish_git_prompt_showdirtystate 1
+set __fish_git_prompt_showuntrackedfiles 1
+set __fish_git_prompt_showcolorhints 1
+
 function fish_prompt
-   printf "%s%s%s \$ " (set_color $fish_color_cwd) $PWD (set_color normal)
+   printf "%s%s%s%s \$ " \
+    (set_color $fish_color_cwd) $PWD (set_color normal) \
+    (fish_git_prompt)
 end
