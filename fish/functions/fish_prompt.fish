de1c6ed8fb3cfdb727ad0a8f32d447ffa971dbb1
Brendan Jackman <jackmanb@chromium.org>
2023-12-17 18:50:40 +0100

    Change prompt branch colour to differ from CWD

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -2,6 +2,7 @@ set __fish_git_prompt_show_informative_status 1
 set __fish_git_prompt_showdirtystate 1
 set __fish_git_prompt_showuntrackedfiles 1
 set __fish_git_prompt_showcolorhints 1
+set __fish_git_prompt_color_branch cyan
 
 function fish_prompt
    printf "%s%s%s%s \$ " \
