de430605a5a03ec377aa32681b5a98ef3c6bf4bf
Brendan Jackman <jackmanb@chromium.org>
2024-12-17 13:20:10 +0000

    spellcheck_commitmsg.sh: Clean up change-id positioning

--- bin/spellcheck_commitmsg.sh
+++ bin/spellcheck_commitmsg.sh
@@ -7,4 +7,7 @@ sed '/^#/d' -i $1
 change_id_line=$(grep "^Change-Id" $1)
 sed '/^Change-Id/d' -i $1
 hunspell $1
+# Remove blank lines from end of file, otherwise if there's a gap between the
+# footers and the change-id, Git gets confused about where the footers start.
+sed -i -e :a -e '/^\n*$/{$d;N;};/\n$/ba' $1
 echo $change_id_line >> $1
