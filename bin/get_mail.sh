3b077882b2f6ae187a47b19aa111950239f4fb23
Brendan Jackman <bhenryj0117@gmail.com>
2025-04-16 17:31:20 +0000

    Finish get_mail.sh

--- bin/get_mail.sh
+++ bin/get_mail.sh
@@ -5,3 +5,5 @@ set -eux
 lei q -I https://lore.kernel.org/all/ -o ~/mail \
     --threads --dedupe=mid --augment \
     '(a:jackmanb@google.com OR a:linux-mm@kvack.org OR a:x86@kernel.org) AND d:2025-04'
+notmuch new
+notmuch_propagate_mute.py
