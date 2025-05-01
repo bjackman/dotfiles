169c3b574a48fa201171573913a9895a3cc0ba56
Brendan Jackman <bhenryj0117@gmail.com>
2025-05-01 13:18:29 +0000

    get_mail.sh: Fix date query
    I dunno why but this doesn't work as documented, only if I use a full
    day specification. Probbaly lei bug. Also add missing '..'.

--- bin/get_mail.sh
+++ bin/get_mail.sh
@@ -4,6 +4,6 @@ set -eux
 
 lei q -I https://lore.kernel.org/all/ -o ~/mail \
     --threads --dedupe=mid --augment \
-    '(a:jackmanb@google.com OR a:linux-mm@kvack.org OR a:x86@kernel.org) AND d:2025-04'
+    '(a:jackmanb@google.com OR a:linux-mm@kvack.org OR a:x86@kernel.org) AND d:2025-04-01..'
 notmuch new
 notmuch_propagate_mute.py
