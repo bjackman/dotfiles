aa59a80fe7122de410491f04a211f218c07abed9
Brendan Jackman <bhenryj0117@gmail.com>
2025-05-02 10:39:12 +0000

    Delete more unused shit

--- userumount.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Unmount an image file mounted with usermount.sh
-
-echo "Unmounting /dev/loop0"
-umount /mnt/loop0 || echo "Couldn't unmount /mnt/loop0"
-echo "Detaching /dev/loop0"
-sync && sync && sync #  :)
-losetup -d /dev/loop0 || echo "Couldn't detach /dev/loop0"
